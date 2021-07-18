d1ea000d4e4050f0dde8baced4c6579d0c9e2451
Naoki Nishiyama <naoki1010nissy@gmail.com>
2021-07-18 11:17:44 +0900

    テスト（VScode)

---
+++ POSSE課題1.js
@@ -0,0 +1,18 @@
+const correctName = {}
+
+
+
+function correct(){
+    a.className = 'correct';
+    d.className = 'comment'
+}
+
+function incorrect1(){
+    b.className = 'incorrect';
+    a.className = 'correct';
+}
+
+function incorrect2(){
+    c.className = 'incorrect';
+    a.className = 'correct';
+}
